e4a00a741ce991ea61726dbc2100abc01875afd3
borisloor06 <boris.loor06@gmail.com>
2024-02-24 15:07:19 -0500

    Add validation

--- backend/src/note/dto/create-note.dto.ts
+++ backend/src/note/dto/create-note.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsString, IsBoolean, IsOptional } from 'class-validator';
+
 export class CreateNoteDto {
+  @IsNotEmpty()
+  @IsString()
   title: string;
+
+  @IsNotEmpty()
+  @IsString()
   content: string;
+
+  @IsBoolean()
+  @IsOptional()
   state?: boolean;
 }
