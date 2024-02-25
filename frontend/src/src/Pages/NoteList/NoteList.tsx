64f29bf93d3892b1ada1738cc0eab32b5f458fd3
borisloor06 <boris.loor06@gmail.com>
2024-02-25 11:34:15 -0500

    Add useEffect hook to reload notes based on type

--- frontend/src/src/Pages/NoteList/NoteList.tsx
+++ frontend/src/src/Pages/NoteList/NoteList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Note from "./Components/Note/Notes";
 import { useNotes } from "./Hooks/Note.hook";
 import { notesInitialState } from "./interfaces/Note.type";
@@ -7,15 +7,29 @@ import { NoteListProps } from "./interfaces/NoteListProps.type";
 export default function NotesList({ type }: NoteListProps) {
   const { notes, refetchNotes } = useNotes([notesInitialState], type);
 
+  useEffect(() => {
+    reloadNotes();
+  }, [type]);
+
   const reloadNotes = () => {
-    refetchNotes();
+    refetchNotes(type);
+  };
+
+  const validNotes = () => {
+    return (
+      notes !== undefined && notes?.some((note) => note !== notesInitialState)
+    );
   };
 
   return (
     <section className="p-8 grid gap-6 grid-cols-1 md:grid-cols-3">
-      {notes.map((note) => (
-        <Note key={note.id} {...note} reload={reloadNotes} />
-      ))}
+      {validNotes() ? (
+        notes.map((note) => (
+          <Note key={note.id} {...note} reload={reloadNotes} />
+        ))
+      ) : (
+        <div className="col-span-full text-center text-2xl">No notes found</div>
+      )}
     </section>
   );
 }
