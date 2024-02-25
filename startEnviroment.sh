ad2b0ac2858fd85058c458e529e41b6f22cf8e5f
borisloor06 <boris.loor06@gmail.com>
2024-02-25 17:16:48 -0500

    Update port variables in startEnviroment.sh

--- startEnviroment.sh
+++ startEnviroment.sh
@@ -13,8 +13,8 @@ then
         export PG_PASS=7xN4bFN89DfW4I0r5+Jugf4xqmaBL2gYM8jT08l0r3SqWijSafupIkrkfwmYE22
         export PG_DB=note-app
         export DB_PORT=5432
-        export BACKEND_PORT=3000        
-        export FRONTED_PORT=3001
+        export BACKEND_PORT=3000
+        export FRONTEND_PORT=3001
         echo "port for backend: $BACKEND_PORT", "port for fronted: $FRONTED_PORT"
         # create dir for volume postgres-data if not exists
         if [ ! -d ./postgres-data ]; then
