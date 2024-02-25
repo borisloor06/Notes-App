6b171a780adb1400b0c0d3b89953501e1c123461
borisloor06 <boris.loor06@gmail.com>
2024-02-25 17:36:24 -0500

    Update Vite config to load environment variables

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig, ServerOptions } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3001,
-  } as ServerOptions,
+export default defineConfig((configEnv) => {
+  const env = loadEnv(configEnv.mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      strictPort: true,
+      port: 3001,
+    },
+    define: {
+      __APP_ENV__: env.APP_ENV,
+      "process.env": env,
+    },
+  };
 });
