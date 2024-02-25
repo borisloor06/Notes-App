309082394ec9508a52c5e0475a1d1811ac4d2825
borisloor06 <boris.loor06@gmail.com>
2024-02-25 10:00:30 -0500

    Services for the notes

---
+++ frontend/src/src/Pages/NoteList/Services/Note.service.ts
@@ -0,0 +1,57 @@
+import { URL_API } from "../../../Constants/constants";
+import { Note } from "../interfaces/Note.type";
+
+export const getNotes = async () => {
+  const response = await fetch(`${URL_API}/note`);
+  const notes = (await response.json()) as Note[];
+
+  return notes;
+};
+
+export const createNote = async (note: Note) => {
+  const response = await fetch(`${URL_API}/note`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(note),
+  });
+
+  return response.json();
+};
+
+export const updateNote = async (note: Note) => {
+  const response = await fetch(`${URL_API}/note/${note.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(note),
+  });
+
+  return response.json();
+};
+
+export const deleteNote = async (id: string) => {
+  const response = await fetch(`${URL_API}/note/${id}`, {
+    method: "DELETE",
+  });
+
+  return response.json();
+};
+
+export const getNote = async (id: string) => {
+  const response = await fetch(`${URL_API}/note/${id}`);
+  const note = (await response.json()) as Note;
+
+  return note;
+};
+
+export const archiveNote = async (id: string, state: boolean) => {
+  const response = await fetch(`${URL_API}/note/archive/${id}`, {
+    method: "PATCH",
+    body: JSON.stringify({ state }),
+  });
+
+  return response.json();
+};
---
+++ frontend/src/src/Pages/NoteList/interfaces/Note.type.ts
@@ -0,0 +1,17 @@
+export interface Note {
+  id?: string;
+  title: string;
+  content: string;
+  state: boolean;
+  created: string;
+  updated: string;
+}
+
+export const notesInitialState = {
+  id: "",
+  title: "",
+  content: "",
+  state: true,
+  created: "",
+  updated: "",
+};
