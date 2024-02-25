d2afffd52626153201fdd57c1054a7348ab64e3f
borisloor06 <boris.loor06@gmail.com>
2024-02-25 12:31:31 -0500

    Update NewNote component path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Layout from "./src/Pages/Layout/Layout";
 import RoutesWithNotFound from "./src/Pages/404/RoutesWithNotFound";
 import NotesList from "./src/Pages/NoteList/NoteList";
-import NewNote from "./src/Pages/Note/NewNote";
+import NewNote from "./src/Pages/NewNote/NewNote";
 import { NoteState } from "./src/Pages/NoteList/interfaces/Note.type";
 
 function App() {
