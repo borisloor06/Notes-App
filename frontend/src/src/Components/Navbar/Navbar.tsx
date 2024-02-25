57644364146f791238da8c15e8e7a2e7794fe800
borisloor06 <boris.loor06@gmail.com>
2024-02-25 11:34:08 -0500

    Update Navbar component to use react-router-dom

--- frontend/src/src/Components/Navbar/Navbar.tsx
+++ frontend/src/src/Components/Navbar/Navbar.tsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <header className="bg-primary text-white p-4 flex justify-between items-center">
-        <h1 className="text-4xl">Notes App</h1>
-        <nav className="flex w-40 justify-around">
-                <div>
-                    <a href="#">Home</a>
-                </div>
-                <div>
-                    <a href="#">Notes</a>
-                </div>
-        </nav>
+      <h1 className="text-4xl">Notes App</h1>
+      <nav className="flex w-40 justify-around">
+        <div>
+          <Link to="/">My notes</Link>
+        </div>
+        <div>
+          <Link to="/archive">Archive</Link>
+        </div>
+      </nav>
     </header>
-  )
+  );
 }
