c16f71e1a2c1c80c2d124676a9c1323a6fdd698e
borisloor06 <boris.loor06@gmail.com>
2024-02-26 02:49:28 -0500

    Fix formatting in note.sql

--- note.sql
+++ note.sql
@@ -5,15 +5,15 @@ CREATE TABLE IF NOT EXISTS notes  (
   state BOOLEAN,
   created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   updated TIMESTAMPTZ NOT NULL DEFAULT NOW()
-)
+);
 
 CREATE TABLE IF NOT EXISTS category (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(255) NOT NULL
-)
+);
 
 create table IF NOT EXISTS note_categories(
 	id SERIAL primary key,
 	id_category int not null,
 	id_note int not null
-)
+);
