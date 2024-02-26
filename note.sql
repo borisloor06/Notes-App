abff5c41d8eba1ce2ad4d3b89e3a650a7c644bbf
borisloor06 <boris.loor06@gmail.com>
2024-02-26 02:48:29 -0500

    Add category and note_categories tables

--- note.sql
+++ note.sql
@@ -6,3 +6,14 @@ CREATE TABLE IF NOT EXISTS notes  (
   created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   updated TIMESTAMPTZ NOT NULL DEFAULT NOW()
 )
+
+CREATE TABLE IF NOT EXISTS category (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(255) NOT NULL
+)
+
+create table IF NOT EXISTS note_categories(
+	id SERIAL primary key,
+	id_category int not null,
+	id_note int not null
+)
