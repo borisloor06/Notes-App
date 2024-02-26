a0b4241b397f9c027ed1aa91ba8a8b32bdd4faa0
borisloor06 <boris.loor06@gmail.com>
2024-02-25 19:36:06 -0500

    Refactor NotFound component

--- frontend/src/src/Pages/404/NotFound.tsx
+++ frontend/src/src/Pages/404/NotFound.tsx
@@ -1,12 +1,14 @@
 function NotFound() {
-	return (
-		<div className="text-center vh-100 b-404">
-			<h1 className="py-2 text-light">Page Not Found</h1>
-			<div>
-				<img width="60%" src="src/assets/404.webp" alt="Page not found" />
-			</div>
-		</div>
-	);
+  return (
+    <div className="text-center vh-100 b-404">
+      <h1 className="py-2 text-light">Page Not Found</h1>
+      <img
+        className="mx-auto w-100"
+        src="src/assets/404.webp"
+        alt="Page not found"
+      />
+    </div>
+  );
 }
 
 export default NotFound;
